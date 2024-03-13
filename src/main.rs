0c14629c37ab94566b33e3c9f4b78e584709fa92
kangseokgyu <ksg890528@gmail.com>
2024-03-14 00:04:37 +0900

    Control Flow

--- src/main.rs
+++ src/main.rs
@@ -51,4 +51,19 @@ fn control_flow() {
         counter += 1;
     };
     println!("number is {number}.");
+
+    let mut counter = 0;
+    while counter < 10 {
+        counter += 1;
+    }
+    println!("counter is {counter}.");
+
+    for i in (1..10) {
+        println!("{i}");
+    }
+
+    let numbers = [10, 20, 30, 40, 50];
+    for number in numbers {
+        println!("{number}");
+    }
 }
