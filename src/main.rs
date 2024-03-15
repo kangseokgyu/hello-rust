b55eb140702942f1f6cfcc9792550d539647b738
kangseokgyu <ksg890528@gmail.com>
2024-03-16 00:37:42 +0900

    Reference and Borrowing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    control_flow();
+    reference_borrowing();
 }
 
 fn guessing_game() {
@@ -66,4 +66,23 @@ fn control_flow() {
     for number in numbers {
         println!("{number}");
     }
+}
+
+fn reference_borrowing() {
+    let mut s1 = String::from("value");
+
+    let r1 = &s1;
+    let r2 = &s1;
+
+    println!("{} {}", r1, r2);
+
+    let r3 = &mut s1;
+    println!("{}", r3);
+
+    /* dangling reference
+    let s2 = {
+        let s = String::from("dangle");
+        &s
+    };
+    */
 }
