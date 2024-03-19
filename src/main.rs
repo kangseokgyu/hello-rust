17e02b44859d6548c156870676f88b05dce70289
kangseokgyu <ksg890528@gmail.com>
2024-03-19 18:33:27 +0900

    Struct

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    slice_type();
+    let rect1 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    dbg!(&rect1);
+
+    let square1 = Rectangle::square(5);
+    dbg!(&square1);
+
+    println!("rect1 size: {}", rect1.area());
+    println!("square1 size: {}", square1.area());
+    println!("Can rect1 hold square1: {}", rect1.can_hold(&square1));
 }
 
 fn guessing_game() {
@@ -104,4 +115,27 @@ fn first_word(s: &str) -> &str {
     }
 
     &s[..]
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Self) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
