11294cc431f4020d2a78f6d796d2579b504b6453
kangseokgyu <ksg890528@gmail.com>
2024-03-20 09:31:48 +0900

    Struct

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,7 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 10,
-        height: 20,
-    };
-    dbg!(&rect1);
-
-    let square1 = Rectangle::square(5);
-    dbg!(&square1);
-
-    println!("rect1 size: {}", rect1.area());
-    println!("square1 size: {}", square1.area());
-    println!("Can rect1 hold square1: {}", rect1.can_hold(&square1));
+    rectangle();
 }
 
 fn guessing_game() {
@@ -138,4 +127,19 @@ impl Rectangle {
     fn can_hold(&self, other: &Self) -> bool {
         self.width > other.width && self.height > other.height
     }
+}
+
+fn rectangle() {
+    let rect1 = Rectangle {
+        width: 10,
+        height: 20,
+    };
+    dbg!(&rect1);
+
+    let square1 = Rectangle::square(5);
+    dbg!(&square1);
+
+    println!("rect1 size: {}", rect1.area());
+    println!("square1 size: {}", square1.area());
+    println!("Can rect1 hold square1: {}", rect1.can_hold(&square1));
 }
